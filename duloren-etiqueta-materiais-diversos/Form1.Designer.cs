b83af8780b6eadaecc1325346ad766f61dc9fc7c
Caio Almeida <34443195+caiovpalmeidadjr@users.noreply.github.com>
2025-11-10 12:10:10 -0300

    #Primeiro commit

---
+++ duloren-etiqueta-materiais-diversos/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace duloren_etiqueta_materiais_diversos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pgBar = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // pgBar
+            // 
+            this.pgBar.Location = new System.Drawing.Point(92, 40);
+            this.pgBar.Name = "pgBar";
+            this.pgBar.Size = new System.Drawing.Size(248, 23);
+            this.pgBar.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(154, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Procurando etiquetas...";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 80);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pgBar);
+            this.Name = "Form1";
+            this.Text = "DULOREN ETIQUETA MATERIAIS DIVERSOS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar pgBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ duloren-etiqueta-materiais-diversos/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace duloren_etiqueta_materiais_diversos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            pgBar.Style = ProgressBarStyle.Marquee;
+            pgBar.MarqueeAnimationSpeed = 50;
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            GerarEtiquetas();
+        }
+
+        private void GerarEtiquetas()
+        {
+            timer1.Stop();
+
+            //rodar a query que busca os dados
+
+            //passas list para processo das etiquetas
+
+            //atualizar etiquetas no AS400
+
+            timer1.Start();
+        }
+    }
+}
