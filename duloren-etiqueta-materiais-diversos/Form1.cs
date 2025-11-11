5df79bb10d038fe7a224db4b92ece23e52d1f971
Caio Almeida <34443195+caiovpalmeidadjr@users.noreply.github.com>
2025-11-11 12:01:24 -0300

    #ajustes

--- duloren-etiqueta-materiais-diversos/Form1.cs
+++ duloren-etiqueta-materiais-diversos/Form1.cs
@@ -2,27 +2,40 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace duloren_etiqueta_materiais_diversos
 {
     public partial class Form1 : Form
     {
         private string caminhoConfiguracao = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        protected String strStringConexaoAS400 = null;
+
+        protected SqlTransaction objTransacao = null;
+
+        protected OleDbTransaction objTransacaoAS400 = null;
+        protected OleDbConnection objConexaoAS400 = null;
+
+        protected SqlConnection objConexao = null;
 
         public Form1()
         {
             InitializeComponent();
 
+            string strAS400 = String.Empty;
+            strAS400 = "Provider=IBMDA400.DataSource.1;Password=duloren;Persist Security Info=True;User ID=MAJORDAO;Data Source=192.168.1.1;";
+
+            strStringConexaoAS400 = strAS400;
+
             pgBar.Style = ProgressBarStyle.Marquee;
             pgBar.MarqueeAnimationSpeed = 50;
-
-
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -37,12 +50,177 @@ namespace duloren_etiqueta_materiais_diversos
             List<DTO.Etiqueta> list = new List<DTO.Etiqueta>();
 
             //rodar a query que busca os dados
+            DataTable objTabela = getEtiquetas();
 
+            foreach (DataRow objLinha in objTabela.Rows)
+            {
+                DTO.Etiqueta etiquetaBD = new DTO.Etiqueta();
+                etiquetaBD.Material = objLinha["CDMATE"].ToString();
+                etiquetaBD.Qtd = objLinha["QTESTQ"].ToString();
+                etiquetaBD.Data = objLinha["DTULEN"].ToString();
+
+                list.Add(etiquetaBD);
+            }
 
             //passas list para processo das etiquetas
             GerarEtiquetaC(list);
 
             timer1.Start();
+        }
+
+        private DataTable getEtiquetas()
+        {
+            String s_SQL = String.Empty;
+
+            try
+            {
+                //s_SQL = @" SELECT PEDIDO, UF, CIDADE, TRANSP, CONFER, VOLUME, MARCA FROM dulprdmest.arqcai WHERE MARCA = '' AND PEDIDO = '213965' ";
+                //s_SQL = @" SELECT EPEDID, EUF, ECIDAD, ETRANS, ECONFE, EVOLUM, EDIA, EMES, EANO FROM dultstmest.ENCOST WHERE EMARCA = '' ";
+                s_SQL = @" SELECT CDMATE, QTESTQ, DTULEN FROM DULTSTMEST.CMT200F1 WHERE CDSTAT = ''";// WHERE EMARCA = ''";// and EPEDID = '248772'";
+
+                return ExecutaLeituraAS400(s_SQL);
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        protected DataTable ExecutaLeituraAS400(String parSQL)
+        {
+            OleDbCommand objComando = null;
+            OleDbDataAdapter objAdaptador = null;
+            DataTable objTabela = null;
+            try
+            {
+                OpenConnectionAS400();
+
+                objComando = new OleDbCommand();
+                objAdaptador = new OleDbDataAdapter();
+                objTabela = new DataTable();
+
+                objComando.Connection = objConexaoAS400;
+                objComando.CommandText = parSQL;
+                objAdaptador.SelectCommand = objComando;
+                objAdaptador.Fill(objTabela);
+
+                return objTabela;
+            }
+            catch (Exception ex)
+            {
+                LogarErro(ex, parSQL);
+
+                //DialogResult dialogResult2 = MessageBox.Show("Erro ao consultar etiquetas no banco, query: " + parSQL + ", informe o administrador!", "Informação", MessageBoxButtons.OK);
+
+                return objTabela;
+
+                //throw ex;
+            }
+            finally
+            {
+                CloseConnectionAS400();
+            }
+        }
+
+        protected void OpenConnectionAS400()
+        {
+            try
+            {
+                if (objConexaoAS400 == null)
+                {
+                    objConexaoAS400 = new OleDbConnection();
+                    objConexaoAS400.ConnectionString = strStringConexaoAS400;
+                    objConexaoAS400.Open();
+                }
+                else
+                {
+                    if (objConexaoAS400.State == ConnectionState.Closed)
+                    {
+                        objConexaoAS400.ConnectionString = strStringConexaoAS400;
+                        objConexaoAS400.Open();
+                    }
+                    else if (objConexao.State == ConnectionState.Open)
+                    {
+                        objConexaoAS400.Close();
+                        objConexaoAS400.ConnectionString = strStringConexaoAS400;
+                        objConexaoAS400.Open();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        protected void CloseConnectionAS400()
+        {
+            try
+            {
+                if (objConexaoAS400 != null)
+                {
+                    objConexaoAS400.Close();
+                    objConexaoAS400.Dispose();
+                }
+                objConexaoAS400 = null;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        private void LogarErro(Exception exc, string erro)
+        {
+            //EscreveBox(string.Format("Foi encontrado um erro em: ", exc.Message.ToString()), txtBox2);
+
+            string logFile = caminhoConfiguracao + @"\log.txt";
+
+            StreamWriter sw = new StreamWriter(logFile, true);
+            sw.WriteLine("********** {0} **********", DateTime.Now);
+
+            sw.WriteLine("Query: " + erro);
+
+            sw.WriteLine("Inner Exception: ");
+
+            if (exc.InnerException != null)
+            {
+                sw.Write("Tipo do Inner Exception: ");
+                sw.WriteLine(exc.InnerException.GetType().ToString());
+                sw.Write("Inner Exception Mensagem: ");
+                sw.WriteLine(exc.InnerException.Message);
+                sw.Write("Inner Source: ");
+                sw.WriteLine(exc.InnerException.Source);
+                if (exc.InnerException.StackTrace != null)
+                {
+                    sw.WriteLine("Inner Stack Trace: ");
+                    sw.WriteLine(exc.InnerException.StackTrace);
+                }
+            }
+            else
+            {
+                sw.WriteLine("Sem informação.");
+            }
+
+            sw.Write("Tipo da Exception: ");
+            sw.WriteLine(exc.GetType().ToString());
+            sw.WriteLine("Exception Mensagem: " + exc.Message);
+            sw.WriteLine("Stack Trace: ");
+
+            if (exc.StackTrace != null)
+            {
+                sw.WriteLine(exc.StackTrace);
+                sw.WriteLine();
+            }
+            else
+            {
+                sw.WriteLine("Sem informação.");
+            }
+
+            sw.Close();
         }
 
         private void GerarEtiquetaC(List<DTO.Etiqueta> list)
